bc4b61a2872dd67daf3271aa9f987c08474edf4f
Samyak jain <samyakjain137@gmail.com>
2025-02-12 19:38:37 +0530

    change footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
-  return (
-    <footer>
-      Designed and Developed by Pariyul
-    </footer>
-  )
-}
+  return <footer>Designed and Developed by Samyak</footer>;
+};
 
-export default Footer
+export default Footer;
