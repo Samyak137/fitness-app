8dc745bd0da513b9ee538f1b4895dbc068b51a3d
Samyak jain <samyakjain137@gmail.com>
2025-02-12 15:31:19 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// Replace 'fitness-app' with your actual GitHub repo name
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/fitness-app/",
+});
